9f456ea880c697468dc4676f194e5d6997c60c2c
milktegi <milkteegi@gmail.com>
2019-01-01 13:59:40 +0900

    Adjusting ui for mobile view

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,7 +2,9 @@ import React from 'react';
 import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.css'
 const logo = props => (
-	<div className={classes.Logo}>
+	<div 
+	style={{height: props.height}}
+	className={classes.Logo}>
 		<img src={burgerLogo} alt="myburger"/>
 	</div>
 );
